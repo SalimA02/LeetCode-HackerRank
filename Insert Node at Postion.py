07584464fcbd2050808d31f84e88f3f5b78ec41e
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2024-08-30 21:41:10 +0100

    Create Insert Node at Postion.py

---
+++ Insert Node at Postion.py
@@ -0,0 +1,15 @@
+def insertNodeAtPosition(llist, data, position):
+    new_node = SinglyLinkedListNode(data)
+    
+    if position == 0:
+        new_node.next = head
+        return new_node
+    
+    current = head
+    for _ in range(position - 1):
+        current = current.next
+    
+    new_node.next = current.next
+    current.next = new_node
+    
+    return head
