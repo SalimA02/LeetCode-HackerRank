ee88a07e9fb6b422d8045a2b3d4409e20dac3d6e
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2024-08-31 20:58:05 +0100

    Create Balanced Brackets - HR.py

---
+++ Balanced Brackets - HR.py
@@ -0,0 +1,13 @@
+def isBalanced(s):
+    stack = []
+    bracket_map = {')': '(', '}': '{', ']': '['}
+    
+    for char in s:
+        if char in bracket_map.values():
+            stack.append(char)
+        elif char in bracket_map:
+            if not stack or stack[-1] != bracket_map[char]:
+                return "NO"
+            stack.pop()
+    
+    return "YES" if not stack else "NO"
