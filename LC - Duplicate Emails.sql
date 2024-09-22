cedc76609df2186d2c22e59f5a1426a3d3cf2041
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2024-09-22 22:20:32 +0100

    Update LC - Duplicate Emails.sql

--- LC - Duplicate Emails.sql
+++ LC - Duplicate Emails.sql
@@ -4,3 +4,21 @@ GROUP BY email
 HAVING COUNT(email) > 1;
 
 
+-- Example 1:
+
+-- Input: 
+-- Person table:
+-- +----+---------+
+-- | id | email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- Output: 
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- Explanation: a@b.com is repeated two times.
