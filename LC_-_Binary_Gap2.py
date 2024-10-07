f7bb25235747f5a3bf194c0ddc711b3195014ea0
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2024-10-07 21:10:06 +0100

    Create LC_-_Binary_Gap2.py

---
+++ LC_-_Binary_Gap2.py
@@ -0,0 +1,10 @@
+def binaryGap(self, n):
+    binary_representation = bin(n)[2:] 
+    zero_sequences = binary_representation.split('1')  
+  
+    if len(zero_sequences) <= 2:
+          return 0  
+      
+    longest_sequence = max(len(seq) for seq in zero_sequences[1:-1])
+    return longest_sequence 
+# finds the longest "0" sequence within "1"s
