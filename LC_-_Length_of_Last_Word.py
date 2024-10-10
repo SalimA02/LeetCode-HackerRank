4a5f5c4d1673401a7ad883a321ccf863597a2161
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2024-10-10 21:08:42 +0100

    Update LC_-_Length_of_Last_Word.py

--- LC_-_Length_of_Last_Word.py
+++ LC_-_Length_of_Last_Word.py
@@ -1,1 +1,13 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        
+        s = s.strip()
 
+        words = s.split()
+
+        lastWord = words[-1]
+        
+        return len(lastWord) 
+      
+
+        
